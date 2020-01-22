b875499652a27e8587ac325ba3b0c927c8656141
Liu Yen-Ting <windows.linux.mac@gmail.com>
2020-01-22 16:57:21 +0800

    Fix python version to 3.7

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     license="Apache 2.0",
     packages=find_packages(),
     package_data={"": ["data/*"]},
+    python_requires="==3.7",
     install_requires=[
         "click",
         "coloredlogs",
