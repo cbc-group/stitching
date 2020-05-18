b658411d4ee1d904e4287205098257df35c3291a
Liu Yen-Ting <windows.linux.mac@gmail.com>
2020-05-18 10:42:43 +0800

    Use utoolbox open_dataset

--- workspace/xml2xl.py
+++ workspace/xml2xl.py
@@ -1,20 +1,30 @@
+import logging
 import os
 import re
 import xml.etree.ElementTree as et
 from collections import defaultdict
 from glob import glob
 
+import coloredlogs
 import numpy as np
 import pandas as pd
 
+from utoolbox.io import open_dataset
+
+logging.getLogger("tifffile").setLevel(logging.ERROR)
+coloredlogs.install(
+    level="DEBUG", fmt="%(asctime)s %(levelname)s %(message)s", datefmt="%H:%M:%S"
+)
+
+
 # load and parse the file
-src_dir = "Y:/ARod/4F/20200324_No5_CamA/preview_CamA_downsample/layer_0"
-src_xml = glob(os.path.join(src_dir, "*.xml"))[0]
+src_dir = "Y:/ARod/4F/20200324_No5_CamA"
+src_xml = "0.xml"
 xtree = et.parse(src_xml)
 xroot = xtree.getroot()
 
 # /Extends/Image
-xroot = xroot.findall('Extends')[0]
+xroot = xroot.findall("Extends")[0]
 
 # pull out attributes
 info = []
@@ -29,57 +39,62 @@ for record in info:
 
 # convert to actual DataFrame
 df = pd.DataFrame.from_dict(records)
- 
+
 # assign proper dtype
 dtype_table = {k: np.float32 for k in df.columns}
 # .. except filename
-dtype_table['Filename'] = str
+dtype_table["Filename"] = str
 df = df.astype(dtype_table)
 
 # calculate actual size
-for ax in ('X', 'Y', 'Z'):
-    df[ax] = (df[f'Max{ax}'] - df[f'Min{ax}']).round().astype(int)  # we know it is discrete
+for ax in ("X", "Y", "Z"):
+    df[ax] = (
+        (df[f"Max{ax}"] - df[f"Min{ax}"]).round().astype(int)
+    )  # we know it is discrete
 
 # calculate tile index
 index = defaultdict(list)
-pattern = r'_(\d+)_x(\d{3})_y(\d{3})'
-for tile_name in df['Filename']:
+pattern = r"_(\d+)_x(\d{3})_y(\d{3})"
+for tile_name in df["Filename"]:
     matches = re.search(pattern, tile_name)
     assert matches is not None, "unable to interpret filename"
 
-    for ax, value in zip(('iZ', 'iX', 'iY'), matches.groups()):
+    for ax, value in zip(("iZ", "iX", "iY"), matches.groups()):
         value = int(value)
         index[ax].append(value)
 # write back
 for key, value in index.items():
-   df[key] = pd.Series(value)
+    df[key] = pd.Series(value)
 
-print('>> BEFORE')
+print(">> BEFORE")
 print(df)
 print()
 
 # convert to full resolution coordinate
-for ax in ('X', 'Y'):
-    df[f'Max{ax}'] = (df[f'Max{ax}'] * 2048 / df['X'])
-    df[f'Min{ax}'] = (df[f'Min{ax}'] * 2048 / df['X'])
+for ax in ("X", "Y"):
+    df[f"Max{ax}"] = df[f"Max{ax}"] * 2048 / df["X"]
+    df[f"Min{ax}"] = df[f"Min{ax}"] * 2048 / df["X"]
 
 # replace 'Filename'
-for 
+ds = open_dataset(src_dir)
+print(ds)
+print(ds[{"tile_x": 0, "tile_y": 0, "tile_z": 0}])
+raise RuntimeError("DEBUG")
+# for i, row in df:
+#    df.at[i, "Filename"] = None
 
-
-
-
-print('>> AFTER')
-print(df)
+print(">> AFTER")
+with pd.option_context("display.max_columns", None):
+    print(df)
 print()
 
 # update attributes
 for node in xroot:
-    filename = node.get('Filename')
-    
+    filename = node.get("Filename")
+
     # lookup updated attributes
-    attributes = df[df['Filename'] == filename]
-    assert len(attributes) == 1, 'duplicated filename'
+    attributes = df[df["Filename"] == filename]
+    assert len(attributes) == 1, "duplicated filename"
     new_attrs = attributes.iloc[0].to_dict()
 
     # replace
@@ -89,5 +104,5 @@ for node in xroot:
 
 # generate result
 fname, fext = os.path.splitext(src_xml)
-dst_xml = f'{fname}_modify{fext}'
+dst_xml = f"{fname}_modify{fext}"
 xtree.write(dst_xml)
